342b1153d6fd6037b0021d615a46f80012a13e78
Rutuja120 <rutujas1204@gmail.com>
2024-04-22 22:17:38 +0530

    third commit

--- nested_loops_demo.py
+++ nested_loops_demo.py
@@ -1,7 +1,7 @@
 #nested loop
 
 for row in range(1, 4, 1):
-    print(row * 2, end="->")
+    print(row , end="->")
     for col in range(1, 5, 1):
-        print(col *2 , end=" ")
+        print(col , end=" ")
     print()
---
+++ pass_demo.py
@@ -0,0 +1,9 @@
+num = 4
+if(num == 1):
+    print("Welcome to MyGames")
+elif(num == 2):
+    pass
+elif(num == 3):
+    print("Byeeee")
+else:
+    print("Invalid choice...!")
